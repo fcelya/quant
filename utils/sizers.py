df36e8e17c3e72cdf89431badf79db80cc2bcb4f
fcelya <92804587+fcelya@users.noreply.github.com>
2022-07-29 04:19:10 +0200

    Added sizers and cleaned up

--- utils/sizers.py
+++ utils/sizers.py
@@ -11,17 +11,37 @@ import backtrader as bt
 
 
 class Ludopata(bt.Sizer):
-    params = (("percent", 1),)
+    params = (("percent", 0.5),)
 
     def __init__(self):
-        self.multiplier = self.params.multiplier
-        print("[WARNING] - Sizer not configured. Buying fixed amount of shares")
+        self.percent = self.params.percent
+
+        # print("[WARNING] - Sizer not configured. Buying fixed amount of shares")
         pass
 
     def _getsizing(self, comminfo, cash, data, isbuy):
-        if isbuy = True and self.
+        position = self.strategy.getposition(data)
+        if isbuy == True and not position:
+            # size = cash * (self.percent - comminfo.commission)
+            size = self.percent * cash / data[0]
+        elif isbuy == False and position:
+            # size = self.position
+            size = position.size
+        else:
+            size = 0
 
-        if isbuy=False and self.position
+        return size
 
-        size = cash  * (self.multiplier - comminfo.commission)
+
+class FixedSize(bt.Sizer):
+    params = (("stake", 1),)
+
+    def _getsizing(self, comminfo, cash, data, isbuy):
+        return self.params.stake
+
+
+class FixedRerverser(FixedSize):
+    def _getsizing(self, comminfo, cash, data, isbuy):
+        position = self.broker.getposition(data)
+        size = self.p.stake * (1 + (position.size != 0))
         return size
